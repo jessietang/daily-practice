9c2c573ef1fe28b865457651675947d1bd877956
jessietang <jessietang@aaxischina.com>
2016-09-12 13:57:13 +0800

    add some previous tests

---
+++ form序列化为json.js
@@ -0,0 +1,24 @@
+/**
+ * Created by rain on 2015/10/3.
+ */
+
+
+(function($){  //’‚—˘”√£∫ console.log($('form').serializeJson());
+    $.fn.serializeJson=function(){
+        var serializeObj={};
+        var array=this.serializeArray();[obje]
+        var str=this.serialize();
+        $(array).each(function(){
+            if(serializeObj[this.name]){
+                if($.isArray(serializeObj[this.name])){
+                    serializeObj[this.name].push(this.value);
+                }else{
+                    serializeObj[this.name]=[serializeObj[this.name],this.value];
+                }
+            }else{
+                serializeObj[this.name]=this.value;
+            }
+        });
+        return serializeObj;
+    };
+})(jQuery);
